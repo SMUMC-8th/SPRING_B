190364f6fb5cf12103041bf48640ccee13a01cde
donghyukseo <ehdgurdusdn@naver.com>
2025-05-08 12:00:27 +0900

    Initial commit

---
+++ src/main/java/umc/teamB/SMUClub/SmuClubApplication.java
@@ -0,0 +1,13 @@
+package umc.teamB.SMUClub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmuClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmuClubApplication.class, args);
+	}
+
+}
