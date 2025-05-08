4d8623b65ebd378dd0e9fc78db97a6ea55166892
donghyukseo <ehdgurdusdn@naver.com>
2025-05-09 01:52:40 +0900

    feat : Entity 생성

---
+++ src/main/java/umc/teamB/SMUClub/domain/faq/entity/Faq.java
@@ -0,0 +1,29 @@
+package umc.teamB.SMUClub.domain.faq.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.teamB.SMUClub.global.entity.BaseEntity;
+
+@Entity
+@Table(name = "faq")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+public class Faq extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    // String → JPA에서 특별한 설정 없으면 DB에서 VARCHAR(255)로 매핑
+    // DB에서는 TEXT 타입으로 매핑되어 약 65,535자까지 저장
+    @Column(columnDefinition = "TEXT")
+    private String question;
+
+    @Column(columnDefinition = "TEXT")
+    private String answer;
+
+}
