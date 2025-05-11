de7078d337d21c609f3145e5027b183d8a42fbd1
noonbbara <ysys1226@naver.com>
2025-05-11 14:31:44 +0900

    ✨ [feat] 동아리 검색 기능 (CLUB 404 추가)

--- src/main/java/umc/teamB/SMUClub/domain/club/service/query/ClubQueryServiceImpl.java
+++ src/main/java/umc/teamB/SMUClub/domain/club/service/query/ClubQueryServiceImpl.java
@@ -2,14 +2,19 @@ package umc.teamB.SMUClub.domain.club.service.query;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import umc.teamB.SMUClub.domain.club.dto.ClubResponseDTO;
 import umc.teamB.SMUClub.domain.club.entity.Club;
+import umc.teamB.SMUClub.domain.club.exception.ClubErrorCode;
+import umc.teamB.SMUClub.domain.club.exception.ClubException;
 import umc.teamB.SMUClub.domain.club.repository.ClubRepository;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ClubQueryServiceImpl implements ClubQueryService {
 
@@ -18,6 +23,10 @@ public class ClubQueryServiceImpl implements ClubQueryService {
     @Override
     public List<ClubResponseDTO> searchClubs(String keyword) {
         List<Club> clubs = clubRepository.searchAllFields(keyword);
+        if (clubs.isEmpty()) {
+            throw new ClubException(ClubErrorCode.NOT_FOUND_404);
+        }
+
         return clubs.stream()
                 .map(ClubResponseDTO::fromEntity)
                 .collect(Collectors.toList());
